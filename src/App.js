c1bcfd54cbb3e6a24cac8ba97e1e691dfa2b8f12
SAI1995PAVAN <saipavankanduri@gmail.com>
2022-10-22 23:08:48 +0530

    correcting error relating to formInput input tag name attribute

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import FormInput from "./Components/FormInput/FormInput";
 
 function App() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    birthday: "",
+    confirmpassword: "",
+  });
+
+  const inputsArray = [
+    {
+      id: 1,
+      idname: "username",
+      type: "text",
+      label: "Username",
+      errorMessage:
+        "should be 3-16 letters and no special charecter and no number",
+
+      pattern: "^[a-zA-Z]{3,16}$",
+    },
+    {
+      id: 2,
+      idname: "email",
+      type: "email",
+      label: "Email",
+      errorMessage: "enter a valid email",
+    },
+    {
+      id: 3,
+      idname: "birthday",
+      type: "date",
+      label: "Birthday",
+      errorMessage: "",
+    },
+    {
+      id: 4,
+      idname: "password",
+      type: "password",
+      label: "Password",
+      errorMessage:
+        "should contain atleast 1-special charecter,1-Capital letter,1-numerical and total length should be minimum 8",
+      pattern: "^(?=.{8,32}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*",
+    },
+    {
+      id: 5,
+      idname: "confirmpassword",
+      type: "password",
+      label: "Confirm Password",
+      errorMessage: "passwords are not matching",
+      pattern: values.password,
+    },
+  ];
+
+  const handleOnChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleOnSubmit = (e) => {
+    setValues({
+      username: "",
+      email: "",
+      password: "",
+      birthday: "",
+      confirmpassword: "",
+    });
+  };
+
+  console.log(values);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleOnSubmit}>
+        <h1>React Form</h1>
+        {inputsArray.map((formInput) => {
+          return (
+            <FormInput
+              key={formInput.id}
+              {...formInput}
+              value={values[formInput.idname]}
+              handleOnChange={handleOnChange}
+            />
+          );
+        })}
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
+//let js-password-regex='^(?=.{8,32}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*';
---
+++ src/Components/FormInput/FormInput.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./FormInput.css";
+
+const FormInput = (props) => {
+  const {
+    handleOnChange,
+    value,
+    idname,
+    errorMessage,
+    label,
+
+    ...formInputProps
+  } = props;
+
+  const [focus, setFocus] = useState(false);
+
+  const handleFocus = () => {
+    setFocus(true);
+  };
+
+  return (
+    <div className="form-input">
+      <label htmlFor={idname}>{label}</label>
+      <input
+        {...formInputProps}
+        onChange={handleOnChange}
+        onBlur={handleFocus}
+        id={idname}
+        focused={focus.toString()}
+        placeholder={idname}
+        name={idname}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
